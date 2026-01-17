6ab2b08745e11ac793d0d739ce785adb8659e6e7
Pedro Henrique <88625441+Pedroqne@users.noreply.github.com>
2026-01-17 12:50:55 -0300

    feat: Add useCase createEvent

--- src/main/java/ex/pedro/EventClean/Core/UseCases/createEventUseCaseImpl.java
+++ src/main/java/ex/pedro/EventClean/Core/UseCases/createEventUseCaseImpl.java
@@ -1,11 +1,19 @@
 package ex.pedro.EventClean.Core.UseCases;
 
 import ex.pedro.EventClean.Core.Entities.Event;
+import ex.pedro.EventClean.Core.Gateway.EventRepositoryPort;
 
 public class createEventUseCaseImpl implements createEventUseCase{
 
+    private final EventRepositoryPort eventGateway;
+
+    public createEventUseCaseImpl(EventRepositoryPort eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
+
     @Override
     public Event execute(Event event) {
-        return null;
+        return eventGateway.save(event);
     }
 }
