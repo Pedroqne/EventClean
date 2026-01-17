e82e20f9d8bf448326313e1e97394343677d6c5e
Pedro Henrique <88625441+Pedroqne@users.noreply.github.com>
2026-01-17 12:52:04 -0300

    feat: Add EventController

--- src/main/java/ex/pedro/EventClean/Infra/Presentation/EventController.java
+++ src/main/java/ex/pedro/EventClean/Infra/Presentation/EventController.java
@@ -1,15 +1,36 @@
 package ex.pedro.EventClean.Infra.Presentation;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import ex.pedro.EventClean.Infra.DTOs.EventDTO;
+import ex.pedro.EventClean.Infra.Gateway.EventRepositoryImpl;
+import ex.pedro.EventClean.Infra.Mappers.EventMapper;
 
 @RestController
 @RequestMapping("api/v1/")
 public class EventController {
 
-    @PostMapping
-    public String createEvent(){
+    private final EventRepositoryImpl eventRepositoryImpl;
+    private final EventMapper eventMapper;
 
+    public EventController(EventRepositoryImpl eventRepositoryImpl, EventMapper eventMapper) {
+        this.eventRepositoryImpl = eventRepositoryImpl;
+        this.eventMapper = eventMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDTO> createEvent(@RequestBody EventDTO eventDTO) {
+        var eventEntity = eventMapper.toDomain(eventDTO);
+
+        var savedEvent = eventRepositoryImpl.save(eventEntity);
+
+        var responseDTO = eventMapper.toDTO(savedEvent);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 }
