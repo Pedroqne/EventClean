da2a0486598e29be32945ad1b5209a45a8013f93
Pedro Henrique <88625441+Pedroqne@users.noreply.github.com>
2026-01-17 12:51:53 -0300

    feat: Implementacao mapper

--- src/main/java/ex/pedro/EventClean/Infra/Mappers/EventMapper.java
+++ src/main/java/ex/pedro/EventClean/Infra/Mappers/EventMapper.java
@@ -1,19 +1,24 @@
 package ex.pedro.EventClean.Infra.Mappers;
 
+import ex.pedro.EventClean.Core.Entities.Event;
 import ex.pedro.EventClean.Infra.DTOs.EventDTO;
 import ex.pedro.EventClean.Infra.Persistence.EventEntity;
 import jakarta.persistence.Entity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.awt.*;
 
 @Mapper(componentModel = "spring")
 public interface EventMapper {
 
-    @Mapping(target = "name", source = "name")
+    Event toDomain(EventDTO eventDTO);
+    Event toDomain(EventEntity eventEntity);
+
+
+    EventEntity toEntity(Event event);
     EventEntity toEntity(EventDTO eventDTO);
 
-    EventDTO toDTO(EventEntity eventEntity);
 
+    EventDTO toDTO(Event event);
+    EventDTO toDTO(EventEntity eventEntity);
 }
